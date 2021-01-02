55b06495ea5f9156ca8bb9528021d5431e1a67aa
1 <1>
2021-01-03 01:03:55 +0400

    FT-3 Добавление сущности

---
+++ Entities/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace FinanceTracking.Entities
+{
+    using Interfaces;
+
+    public class Product : IHaveId
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public decimal Quantity { get; set; }
+        public decimal Price { get; set; }
+        public string Total => (Quantity * Price).ToString("F2");
+    }
+}
