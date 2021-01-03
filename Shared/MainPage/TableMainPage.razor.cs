0633dec603ff1cab93b905a7a22874634195a94b
1 <1>
2021-01-03 19:19:41 +0400

    FT-3 Чистка кода

---
+++ Shared/MainPage/TableMainPage.razor.cs
@@ -0,0 +1,20 @@
+﻿namespace FinanceTracking.Shared.MainPage
+{
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using Entities;
+    using System.Linq;
+
+    public partial class TableMainPage
+    {
+        public static ObservableCollection<Product> Products { get; } = new();
+
+        public TableMainPage()
+        {
+            Products.CollectionChanged += (_, _) =>
+            {
+                this.StateHasChanged();
+            };
+        }
+    }
+}
