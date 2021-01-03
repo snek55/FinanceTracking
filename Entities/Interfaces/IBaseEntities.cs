82f6ce65567ac4e2d7736f80fbbb92a62dd999df
1 <1>
2021-01-03 19:17:20 +0400

    FT-3 Добавление базового интерфейса

---
+++ Entities/Interfaces/IBaseEntities.cs
@@ -0,0 +1,10 @@
+﻿namespace FinanceTracking.Entities.Interfaces
+{
+    /// <summary>
+    /// Базовый интерфейс для основных сущностей программы
+    /// </summary>
+    public interface IBaseEntities : IHaveId
+    {
+        
+    }
+}
---
+++ Extensions/BaseEntitiesExtension.cs
@@ -0,0 +1,15 @@
+﻿namespace FinanceTracking.Extensions
+{
+    using System.Text.Json;
+    using Entities.Interfaces;
+
+    public static class BaseEntitiesExtension
+    {
+        public static T Clone<T>(this IBaseEntities baseEntities)
+        {
+            var str = JsonSerializer.Serialize(baseEntities, typeof(T));
+
+            return (T) JsonSerializer.Deserialize(str, typeof(T));
+        }
+    }
+}
