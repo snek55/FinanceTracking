79d44814e77b65f2e2e18adcca2b7d93538b65fa
1 <1>
2021-01-03 19:18:29 +0400

    FT-3 Комментарии

--- Entities/Product.cs
+++ Entities/Product.cs
@@ -2,12 +2,29 @@
 {
     using Interfaces;
 
-    public class Product : IHaveId
+    public class Product : IBaseEntities
     {
+        /// <inheritdoc/>>
         public long Id { get; set; }
+
+        /// <summary>
+        /// Имя
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// Количество
+        /// </summary>
         public decimal Quantity { get; set; }
+
+        /// <summary>
+        /// Цена
+        /// </summary>
         public decimal Price { get; set; }
+
+        /// <summary>
+        /// Итоговая сумма
+        /// </summary>
         public string Total => (Quantity * Price).ToString("F2");
     }
 }
