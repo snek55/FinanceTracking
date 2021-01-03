80ee93a2b2b374d00511e121af145a1e26d8ad37
1 <1>
2021-01-03 19:20:31 +0400

    FT-3 Форма добавления записей в таблицу

---
+++ Shared/MainPage/EditFormMainPage.razor.cs
@@ -0,0 +1,15 @@
+﻿namespace FinanceTracking.Shared.MainPage
+{
+    using Entities;
+    using Extensions;
+
+    public partial class EditFormMainPage
+    {
+        private readonly Product _product = new();
+
+        private void HandleValidSubmit()
+        {
+            TableMainPage.Products.Add(this._product.Clone<Product>());
+        }
+    }
+}
