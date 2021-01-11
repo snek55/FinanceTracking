fe7d7efecbbb270bf531547662664bf6fa765509
Aleksandr <nexoposho@gmail.com>
2021-01-11 17:34:24 +0400

    Added class "Value" with its corresponding properties

---
+++ Entities/Value.cs
@@ -0,0 +1,20 @@
+﻿namespace FinanceTracking.Entities
+{
+	using Interfaces;
+	/// <summary>
+	/// Валюта
+	/// </summary>
+	public class Value : IBaseEntity
+	{
+		/// <inheritdoc/>
+		public long Id { get; set; }
+		/// <summary>
+		/// Название валюты
+		/// </summary>
+		public string Name { get; set; }
+		/// <summary>
+		/// Знак валюты, добавляющийся в таблицу
+		/// </summary>
+		public char Symbol { get; set; }
+	}
+}
