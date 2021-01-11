08bf4ae0a48f23c729a69441c2915242b5c83e1b
Aleksandr <nexoposho@gmail.com>
2021-01-11 09:46:46 +0400

    Changes required by Vladimir

--- Pages/ColorPicker.razor.cs
+++ Pages/ColorPicker.razor.cs
@@ -1,8 +1,4 @@
 ﻿using Microsoft.AspNetCore.Components;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Blazored.LocalStorage;
 
 namespace FinanceTracking.Pages
@@ -10,38 +6,26 @@ namespace FinanceTracking.Pages
     public partial class ColorPicker
     {
         private string defaultcolor = "#ffffff";
-        private string commBackgroundColor;
-        private string fontBackgroundColor;
-        private string fontForegroundColor;
 
         [Inject]
         private ISyncLocalStorageService localStorage { get; set; }
-        public string CommBackgroundColor
+
+        private string CommBackgroundColor
         {
             get => GetFromLocalStorage(nameof(CommBackgroundColor));
-            set
-            {
-                commBackgroundColor = value;
-                UpdateLocalStorage(nameof(CommBackgroundColor), value);
-            }
+            set => UpdateLocalStorage(nameof(CommBackgroundColor), value);
         }
+
         private string FontBackgroundColor 
         {
             get => GetFromLocalStorage(nameof(FontBackgroundColor));
-            set 
-            { 
-                fontBackgroundColor = value; 
-                UpdateLocalStorage(nameof(FontBackgroundColor),value); 
-            }
+            set => UpdateLocalStorage(nameof(FontBackgroundColor),value); 
         }
+
         private string FontForegroundColor 
         {
             get => GetFromLocalStorage(nameof(FontForegroundColor));
-            set
-            {
-                fontForegroundColor = value;
-                UpdateLocalStorage(nameof(FontForegroundColor), value);
-            }
+            set => UpdateLocalStorage(nameof(FontForegroundColor), value);
         }
 
         private void UpdateLocalStorage(string k, string v)
@@ -51,9 +35,7 @@ namespace FinanceTracking.Pages
 
         private string GetFromLocalStorage(string k)
         {
-            if(localStorage.ContainKey(k))
-                return localStorage.GetItem<string>(k).ToString();
-            return defaultcolor;
+            return localStorage.ContainKey(k)? localStorage.GetItem<string>(k): defaultcolor;
         }
     }
 }
