7aa2bc8859b15ee5497cda0238eaa57be5255aba
Yaroslav <snek44@gmail.com>
2022-03-13 18:55:26 +0400

    FT-4 Переименована настройка

---
+++ Shared/Settings/CurrencySettings.razor.cs
@@ -0,0 +1,16 @@
+﻿namespace FinanceTracking.Shared.Settings;
+
+using Entities;
+using Enums;
+using Microsoft.AspNetCore.Components;
+
+public partial class CurrencySettings
+{
+	private Currencies CurrentCurrency
+	{
+		get => this.Currency.CurType;
+		set => this.Currency.CurType = value;
+	}
+
+	[Inject] private Currency Currency { get; set; }
+}
