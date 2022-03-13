95f71ee3138488e712a9956a4db09e0f558451a8
Yaroslav <snek44@gmail.com>
2022-03-13 18:18:23 +0400

    FT-4 cleanup code

--- Entities/Currency.cs
+++ Entities/Currency.cs
@@ -1,29 +1,30 @@
-﻿namespace FinanceTracking.Entities
+﻿namespace FinanceTracking.Entities;
+
+using Enums;
+using Interfaces;
+
+/// <summary>
+///     Тип валюты
+/// </summary>
+public class Currency : IBaseEntity
 {
-	using FinanceTracking.Entities.Interfaces;
-	using FinanceTracking.Enums;
+	/// <summary>
+	///     Полное имя типа валюты
+	/// </summary>
+	public string LongName { get; set; }
 
 	/// <summary>
-	/// Тип валюты
+	///     Символ типа валюты ($)
 	/// </summary>
-	public class Currency : IBaseEntity
-	{
-		/// <summary>
-		/// Ид
-		/// </summary>
-		public long Id { get; set; }
-		/// <summary>
-		/// Полное имя типа валюты
-		/// </summary>
-		public string LongName { get; set; }
-		/// <summary>
-		/// Короткое имя типа валюты (EUR)
-		/// </summary>
-		public string ShortName { get; set; }
-		/// <summary>
-		/// Символ типа валюты ($)
-		/// </summary>
-		public char Symbol { get; set; }
-		public Currencies CurType { get; set; }
-	}
+	public char Symbol { get; set; }
+
+	/// <summary>
+	///     Тип валюты
+	/// </summary>
+	public Currencies CurType { get; set; }
+
+	/// <summary>
+	///     Ид
+	/// </summary>
+	public long Id { get; set; }
 }
