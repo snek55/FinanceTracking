877c8e36512792a9856d5a133dbbc8ab0b99b776
Yaroslav Dobosh <snek44@gmail.com>
2023-04-22 19:39:43 +0200

    Settings page and select currency

---
+++ FinanceTracking.App/Views/SettingsPage.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceTracking.App.Views;
+
+public partial class SettingsPage : ContentPage
+{
+	public SettingsPage()
+	{
+		InitializeComponent();
+	}
+}
