cd860ec97fae03cf4e37018bf59c4aba19511d2d
Yaroslav Dobosh <snek44@gmail.com>
2023-04-23 20:49:51 +0200

    Add view object on page

--- FinanceTracking.App/Views/SettingsPage.xaml.cs
+++ FinanceTracking.App/Views/SettingsPage.xaml.cs
@@ -1,15 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace FinanceTracking.App.Views;
+
+using ViewModels;
 
 public partial class SettingsPage : ContentPage
 {
+
 	public SettingsPage()
 	{
 		InitializeComponent();
+
+		this.InitializeComponent();
+
+		this.BindingContext = new SettingsPageViewModel();
 	}
 }
