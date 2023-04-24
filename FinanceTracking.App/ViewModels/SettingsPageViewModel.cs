521621e32b8e02085c6cbcb96bd7ed2a11855575
Yaroslav Dobosh <snek44@gmail.com>
2023-04-24 22:05:18 +0200

    add setting for choose language

--- FinanceTracking.App/ViewModels/SettingsPageViewModel.cs
+++ FinanceTracking.App/ViewModels/SettingsPageViewModel.cs
@@ -8,16 +8,28 @@ public class SettingsPageViewModel : BindableObject
 	public static readonly BindableProperty DefaultCurrencyProperty = BindableProperty.Create(
 		nameof(DefaultCurrency), typeof(string), typeof(SettingsPageViewModel));
 
+	[JsonIgnore]
+	public static readonly BindableProperty DefaultLanguageProperty = BindableProperty.Create(
+		nameof(DefaultLanguage), typeof(string), typeof(SettingsPageViewModel));
+
 	public string DefaultCurrency
 	{
 		get => (string)this.GetValue(DefaultCurrencyProperty);
 		set => this.SetValue(DefaultCurrencyProperty, value);
 	}
 
-	public List<string> Currencies { get; } = new() { "Euro", "US Dollar" };
+	public string DefaultLanguage
+	{
+		get => (string)this.GetValue(DefaultLanguageProperty);
+		set => this.SetValue(DefaultLanguageProperty, value);
+	}
+
+	public List<string> Currencies { get; set; } = new() { "Euro", "US Dollar" };
+	public List<string> Languages { get; set; } = new() { "English" };
 
 	public SettingsPageViewModel()
 	{
 		this.DefaultCurrency = this.Currencies.First();
+		this.DefaultLanguage = this.Languages.First();
 	}
 }
