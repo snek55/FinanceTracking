68abbad24d31d28dae3c94b4536b550de060303d
Yaroslav Dobosh <snek44@gmail.com>
2023-05-16 23:31:00 +0200

    registration services

--- FinanceTracking.App/MauiProgram.cs
+++ FinanceTracking.App/MauiProgram.cs
@@ -1,10 +1,13 @@
-﻿namespace FinanceTracking.App;
-
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
 using DevExpress.Maui;
+using FinanceTracking.App.Views;
+using FinanceTracking.DataBase;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanceTracking.App;
 
 public static class MauiProgram
-{
+{ 
 	public static MauiApp CreateMauiApp()
 	{
 		var builder = MauiApp.CreateBuilder();
@@ -15,12 +18,41 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+			})
+			.InitServices();
 
 #if DEBUG
 		builder.Logging.AddDebug();
 #endif
 
 		return builder.Build();
+	}
+
+	private static void InitServices(this MauiAppBuilder builder)
+	{
+		builder.Services.AddSingleton<MainPage>();
+		builder.Services.AddDbContext<TestDbContext>(option =>
+			option.UseSqlite($"FileName={GetStr()}",
+				x => x.MigrationsAssembly(nameof(DataBase))));
+	}
+
+	private static string GetStr()
+	{
+		var dataBasePath = "";
+		var dataBaseName = "FinanceTrackingDb.db3";
+
+		if (DeviceInfo.Platform == DevicePlatform.Android)
+		{
+			dataBasePath = Path.Combine(FileSystem.AppDataDirectory, dataBaseName);
+		}
+
+		if (DeviceInfo.Platform == DevicePlatform.iOS)
+		{
+			SQLitePCL.Batteries_V2.Init();
+			dataBasePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "..",
+				"Library", dataBaseName);
+		}
+
+		return dataBasePath;
 	}
 }
