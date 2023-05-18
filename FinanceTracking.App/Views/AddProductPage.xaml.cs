4e085b1810f1a24993b2ff403ebf1d4fab57752e
Yaroslav Dobosh <snek44@gmail.com>
2023-05-18 17:59:14 +0200

    init add form

--- FinanceTracking.App/Views/AddProductPage.xaml.cs
+++ FinanceTracking.App/Views/AddProductPage.xaml.cs
@@ -1,9 +1,39 @@
+using FinanceTracking.DataBase;
+using FinanceTracking.DataBase.Entities;
+
 namespace FinanceTracking.App.Views;
 
 public partial class AddProductPage : ContentPage
 {
-    public AddProductPage()
+    public FinanceTrackingDbContext _dbContext { get; set; }
+    
+    private Product _product { get; set; }
+
+    public AddProductPage(FinanceTrackingDbContext dbContext)
     {
         InitializeComponent();
+
+        _dbContext = dbContext;
+        _product = new Product();
+
+        InitFields(_product.IsSolid);
+    }
+
+    private void OnCheckBoxCheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+        _product.IsSolid = e.Value;
+        InitFields(e.Value);
+    }
+
+    private void InitFields(bool isSolid)
+    {
+        SolidBox.IsChecked = isSolid;
+        MeasureLabel.Text = isSolid ? "Weight:" : "Volume:";
+        MeasureType.Text = isSolid ? "gram" : "milliliters";
+    }
+
+    private void OnSaveButtonClicked(object sender, EventArgs e)
+    {
+        throw new NotImplementedException();
     }
 }
